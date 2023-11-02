7d557eb8d4f0151dc05190516f65540b3fe40dfe
Siddharth Singh <sidsingh264@gmail.com>
2023-11-02 11:44:23 +0530

    created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Header from './components/Header'
+
 function App() {
   return (
-     <div>Hello World!</div>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/profile' element={<Profile/>} />
+      <Route path='/signin' element={<SignIn/>} />
+      <Route path='/signup' element={<SignUp/>} />
+    </Routes>
+    </BrowserRouter>
+    
   )
 }
 
