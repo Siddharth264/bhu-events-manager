accc8d152f3e6c986d3ee42fd8fee47add091249
Shruti Gupta <shrurose73@gmail.com>
2023-11-09 18:48:23 +0530

    major styling changes in sign-in and sign-up pages

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,14 +1,32 @@
+import { useState } from 'react'
 import {Link} from 'react-router-dom'
 export default function SignIn() {
+  const [formState, setFormState]= useState({
+   email:"", password:""
+  })
+ 
+const handleChange=(event) => {
+setFormState(prevState=>{
+  return{
+  ...prevState,
+  [event.target.id] : event.target.value
+  }
+}
+)
+}
+const handleSubmit=(event)=>{
+  event.preventDefault()
+  console.log(formState)
+}
   return (
-    <div className="max-w-lg mx-auto mt-32 bg-teal-100 p-10 rounded-lg">
+    <div className="max-w-lg mx-auto mt-32 bg-white  shadow-2xl p-10 rounded-lg">
       <h1 className="text-center text-2xl mb-10 uppercase">Sign In</h1>
-      <form className="flex flex-col gap-3" >
+      <form onSubmit={handleSubmit} className="flex flex-col gap-3" >
         
         <div className="flex flex-col gap-1"><span className="text-sm font-serif tracking-wider text-gray-600 font-medium">Email</span>
-        <input className="rounded-lg border-neutral-300 p-2 focus: outline-none focus:ring-0  focus:border-2 focus:shadow-lg focus:border-gray-400"type="email" id="email" /></div>
+        <input className="rounded-lg border-neutral-300 p-2 focus: outline-none focus:ring-0  focus:border-2 focus:shadow-lg border-2 focus:border-gray-400"type="email" id="email"  onChange={handleChange} /></div>
         <div className="flex flex-col gap-1"><span className="text-sm font-serif tracking-wider text-gray-600 font-medium">Password</span>
-        <input className="rounded-lg border-neutral-300 p-2 focus: outline-none focus:ring-0  focus:border-2 focus:shadow-lg focus:border-gray-400" type="password" id="email" /></div>
+        <input className="rounded-lg border-neutral-300 p-2 focus: outline-none focus:ring-0  focus:border-2 focus:shadow-lg border-2 focus:border-gray-400" type="password" id="password"  onChange={handleChange} /></div>
         <button className="bg-slate-600 p-3 text-white rounded-lg uppercase my-6">Submit</button>
 
 
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,19 +1,38 @@
+import { useState } from 'react';
 import {Link} from 'react-router-dom'
 
 export default function SignUp() {
+  const [formState, setFormState]= useState({
+    userName:"", email:"", password:""
+  })
+ 
+const handleChange=(event) => {
+setFormState(prevState=>{
+  return{
+  ...prevState,
+  [event.target.id] : event.target.value
+  }
+}
+)
+}
+const handleSubmit=(event)=>{
+  event.preventDefault()
+  console.log(formState)
+}
+  
   
   return (
-    <div className="max-w-lg mx-auto mt-32 bg-teal-100 p-10 rounded-lg">
+    <div className="max-w-lg mx-auto mt-32 bg-white border-slate-500 shadow-2xl p-10 rounded-lg">
       <h1 className="text-center text-2xl mb-10 uppercase">Sign up</h1>
-      <form className="flex flex-col gap-3" >
+      <form  onSubmit={handleSubmit} className="flex flex-col gap-3" >
         <div className=" flex flex-col gap-1">
         <span className="text-sm font-serif tracking-wider text-gray-600 font-medium">Username</span>
-        <input className="rounded-lg border-neutral-300 p-2  focus: outline-none focus:ring-0  focus:border-2 focus:shadow-lg focus:border-gray-400" type="text" id="email" /></div>
+        <input className="rounded-lg  p-2  focus: outline-none focus:ring-0  focus:border-2 focus:shadow-lg border-2 focus:border-gray-400" type="text" id="userName"  onChange={handleChange} /></div>
         <div className="flex flex-col gap-1"><span className="text-sm font-serif tracking-wider text-gray-600 font-medium">Email</span>
-        <input className="rounded-lg border-neutral-300 p-2 focus: outline-none focus:ring-0  focus:border-2 focus:shadow-lg focus:border-gray-400"type="email" id="email" /></div>
-        <div className="flex flex-col gap-1"><span className="text-sm font-serif tracking-wider text-gray-600 font-medium">Password</span>
-        <input className="rounded-lg border-neutral-300 p-2 focus: outline-none focus:ring-0  focus:border-2 focus:shadow-lg focus:border-gray-400" type="password" id="email" /></div>
-        <button className="bg-slate-600 p-3 text-white rounded-lg uppercase my-6">Submit</button>
+        <input className="rounded-lg border-neutral-300 p-2 focus: outline-none focus:ring-0   focus:border-2 focus:shadow-lg border-2 focus:border-gray-400"type="email" id="email" onChange={handleChange} /></div>
+        <div className="flex flex-col gap-1"><span className="text-sm font-serif tracking-wider  text-gray-600 font-medium">Password</span>
+        <input className="rounded-lg border-neutral-300 p-2 focus: outline-none focus:ring-0  focus:border-2 focus:shadow-lg border-2 focus:border-gray-400" type="password" id="password"  onChange={handleChange}/></div>
+        <button className="bg-slate-600 p-3 text-white rounded-lg uppercase my-6">Submit</button> 
 
 
       </form>
