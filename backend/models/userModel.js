4e5c5c400e6e22d272ff42dfaed26c3b3f4a80bc
Shruti Gupta <shrurose73@gmail.com>
2024-01-03 18:32:03 +0530

    home page sections added

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose')
 
-const defaultAvatar = "https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o="
-
+const defaultAvatar = "https://i.pngimg.me/thumb/f/720/compngwingmfrjz.jpg"
 const userSchema = new mongoose.Schema({
     username : {
         type: String,
---
+++ client/src/components/Card.jsx
@@ -0,0 +1,34 @@
+
+
+import { Link } from 'react-router-dom';
+import date from '../assets/date.svg'
+import location from '../assets/location.svg'
+
+export default function Card(props) {
+  return (
+    <div className=" bg-white border-2 rounded-xl mx-4 my-6 shadow-md h-[18rem] min-w-[14rem] max-w-[14rem] transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-105 hover:shadow-2xl duration-200 ">
+      <div className="w-full  rounded-xl relative ">
+        <img src={props.imageUrl} className="w-full rounded-t-xl h-[9rem] object-cover" alt="" />
+        <div className="bg-white w-20  text-sm font-semibold absolute top-2 right-2 border rounded-md text-center text-slate-900">{props.category}</div>
+      </div>
+      <div className=" flex flex-col gap-3 p-3">
+        <span className="text-md font-semibold text-slate-800 line-clamp-1 uppercase">{props.title}</span>
+        <div className="flex flex-col gap-1">
+        <div className="flex gap-2 text-xs">
+            <img src={date} alt="" className="w-[1.1rem]"/>
+            <span>{props.date} </span>
+
+        </div>
+        <div className="flex gap-2 text-xs" >
+            <img src={location} alt="" className="w-[1.1rem]"/>
+            <span className="line-clamp-1"> {props.venue} </span>
+
+        </div>
+        </div>
+        <Link to={props.registerUrl}>
+    <button className="border-2 bg-[#b23a48] text-white p-1 text-xs w-[5rem] rounded-md  hover:opacity-90">Register</button>
+    </Link>
+      </div>
+    </div>
+  );
+}
