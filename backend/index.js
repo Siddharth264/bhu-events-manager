6653dbef8134ab93fe2b7c26f0410eb5092a2bc9
Siddharth Singh <sidsingh264@gmail.com>
2024-02-07 03:31:06 +0530

    add pre deployment code

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,11 @@ const userRoute = require('./routes/userRoute')
 const authRoute = require('./routes/authRoute')
 const eventRoute = require('./routes/eventRoute')
 const errorHandler = require('./middleware/errorHandler')
+const path = require("path")
 connectDb()
+
+const _dirname = path.resolve();
+
 
 const app = express();
 
@@ -19,4 +23,8 @@ app.use(express.json())
 app.use('/api/v1/user', userRoute)
 app.use('/api/v1/auth', authRoute)
 app.use('/api/v1/event',eventRoute)
+app.use(express.static(path.join(_dirname,'/client/dist')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(_dirname,'client','dist', 'index.html'));
+  })
 app.use(errorHandler)
